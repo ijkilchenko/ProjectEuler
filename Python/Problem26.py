25aaf4deb0ee4324df72069df516ff76aa43c4a6
Alexey Ilchenko <ijkilchenko@gmail.com>
2014-12-24 19:39:38 -0500

    Solving 26. Made a list of whole parts.

--- Python/Problem26.py
+++ Python/Problem26.py
@@ -5,22 +5,45 @@ Problem 26 of Project Euler
 import math
 
 def run():
-    d= 2
-    while (d < 3):
-        l= []
-        base= 10
-        ##while (
-        if (base/d==0):
-            while (base/d==0):
-                base= base*10
-                l.append(0)
-        else:
-            
+    longest = getDecimalFraction(2)
+    d = 3
+    while (d < 1000):
+        current = getDecimalFraction(d)
+        if (current >= longest) :
+            longest = current
+            print(d)
+            print(longest)
+        d = d + 1
+    
+    
+    return
+    
+def getDecimalFraction(d):
+    p = 10
+    lastR = p % d
+    l =[]
+    newElem = math.floor(p/d)
+    l.append(newElem)
+    #print(l[-1])
+    p = lastR * 10
+    
+    while (lastR != 0):
+        lastR = p % d
+        newElem = math.floor(p/d)
+        try:
+            index = l.index(newElem)
+            return (len(l) - index)
+        except ValueError:
+            l.append(newElem)
+            #print(l[-1])
+            p = lastR * 10
 
-            
-        print(l)
-        d += 1
-
-            
+    try:
+        newElem = math.floor(p/d)
+        index = l.index(newElem)
+        return (len(l) - index)
+    except ValueError:
+        return 0
     
 run()
+
