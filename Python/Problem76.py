0a0e36ab07099320bafc804709b5fc49bcf594a2
Alex Ilchenko <ijkilchenko@gmail.com>
2018-09-19 23:47:14 -0700

    Attempt to speed up Problem 76

---
+++ Python/Problem76.py
@@ -0,0 +1,37 @@
+from math import floor
+
+cache = {}
+
+def get_parts(n):
+  # Returns the partitions of n
+  if n in cache:
+    return cache[n]
+  
+  if n == 1:
+    return [(1,)]
+  if n == 2:
+    return [(1, 1)]
+  
+  # Recursive step
+  partitions = set()
+  for i in range(1, n):
+    if n-i >= i:
+      partitions.add((n-i, i))
+    for part in get_parts(n-i):
+      if part[0] > i:
+        break
+      else:
+        partitions.add((i,) + part)
+  
+  cache[n] = sorted(list(partitions), key=lambda x: x[0])
+  
+  return cache[n]
+
+
+def get_num_parts(n):
+  all_parts = get_parts(n)
+  return len(all_parts)
+
+if __name__ == '__main__':
+  print(get_num_parts(70))
+
